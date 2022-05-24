aa5a01342845fea5b279063f753f8f8f9ca2ebbd
dev-sibli <siblisadik13@gmail.com>
2022-05-24 11:54:10 +0600

    server initialised

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbsjc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('DB Connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Jantrik Tools Server is on')
+})
+
+app.listen(port, () => {
+    console.log(`Jantrik Tools listening on port ${port}`)
+})
