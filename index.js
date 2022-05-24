85613a5afe5f3e25fae5c8dae236798d6143bee6
dev-sibli <siblisadik13@gmail.com>
2022-05-24 23:10:32 +0600

    user creation process added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +17,7 @@ async function run() {
     try {
         await client.connect()
         const toolCollection = client.db('jantrik-tools').collection('tools');
+        const userCollection = client.db('jantrik-tools').collection('users');
 
         app.get('/tool', async (req, res) => {
             const query = {};
@@ -23,6 +25,17 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
         });
+
+        app.put('/user/:email', (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
     }
     finally {
 
