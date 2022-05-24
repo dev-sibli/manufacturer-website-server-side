01c11c11cd4baef71fbb965d1d3866981da351ac
dev-sibli <siblisadik13@gmail.com>
2022-05-25 00:56:43 +0600

    jwt token created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -26,15 +27,18 @@ async function run() {
             res.send(tools);
         });
 
-        app.put('/user/:email', (req, res) => {
+
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
+            const user = req.body;
             const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         })
     }
     finally {
